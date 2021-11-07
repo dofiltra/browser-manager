06f3db7a06ef7ef45f42c50d70cccc6d5f231638
memanot <menemty@gmail.com>
2021-11-07 15:58:25 +0200

    .

--- src/debug.ts
+++ src/debug.ts
@@ -3,21 +3,36 @@ import { BrowserManager } from '.'
 
 const debug = async () => {
   try {
+    const proxies = [
+      { url: 'http://45.89.19.21:16739@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.117:17807@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.50:7167@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.18.237:8135@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.46:4919@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.51:11939@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.63:16725@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.12:18473@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.115:12069@FSOfa5:EZaEVDGtbm' },
+      { url: 'socks5://45.89.19.118:4099@FSOfa5:EZaEVDGtbm' }
+    ]
+
     const pwrt: BrowserManager | null = await BrowserManager.build({
       browserType: chromium,
       launchOpts: {
-        headless: true
+        headless: false,
       },
       device: devices['Pixel 5'],
       idleCloseSeconds: 60,
       maxOpenedBrowsers: 5
+
       // appPath: App.rootPath
     })
 
     // console.log('pwrt', pwrt)
 
-    const page = await pwrt?.newPage({})
-    await page?.goto('https://dofiltra.com')
+    const page = await pwrt?.newPage({
+      url: 'https://youtube.com'
+    })
     // console.log('page', page)
 
     return { pwrt, page }
